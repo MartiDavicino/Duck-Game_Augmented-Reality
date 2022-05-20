599db30190cc87374d26b23237798d40037f66f8
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2022-05-20 16:15:26 +0200

    Begining duck behaviour

---
+++ Duck Game/Assets/Scripts/DuckBehaviour.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
+
+
+public class DuckBehaviour : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    [Range(0, 100)]
+    public float speed;
+    [Range(0,500)]
+    public float radius;
+
+    void Start()
+    {
+        agent=GetComponent<NavMeshAgent>(); 
+        if(agent==null)
+        {
+            agent.speed= speed;
+            agent.SetDestination(RandomNavMeshLocation());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent!=null && agent.remainingDistance<=agent.stoppingDistance)
+        {
+            agent.SetDestination(RandomNavMeshLocation());
+        }
+    }
+
+    public Vector3 RandomNavMeshLocation()
+    {
+        Vector3 finalPos = Vector3.zero;
+        Vector3 randomPos = Random.insideUnitSphere * radius;
+
+        randomPos+=transform.position;
+
+        if(NavMesh.SamplePosition(randomPos,out NavMeshHit hit, radius,1))
+        {
+            finalPos=hit.position;
+        }
+
+        return finalPos;
+    }
+}
+
