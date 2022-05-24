5d92a93513bf2c55898ff2390f22dfb81338706d
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2022-05-24 16:27:35 +0200

    Imprving fishing mechanic

--- Ducking/Assets/Scripts/CameraController.cs
+++ Ducking/Assets/Scripts/CameraController.cs
@@ -8,25 +8,50 @@ public class CameraController : MonoBehaviour
     public float mouseSensitivity;
 
     GameObject UI;
+    GameObject rodHandle;
+    float rodRotationSpeed;
+    [Range(0.001f, .1f)]
+    public float rodRotationIncrease;
+    float rodRotationDecrease;
+
     // Start is called before the first frame update
     void Start()
     {
         UI = GameObject.Find("UI");
         if (UI == null)
             Debug.LogWarning("Couldnt find UI");
+
+        rodHandle = GameObject.Find("handle");
+        if (rodHandle == null)
+            Debug.LogWarning("Couldnt find rod");
+
+        rodRotationSpeed = 0f;
+        rodRotationDecrease = rodRotationIncrease * 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotateHandle();
 
         GetInputLookRotation();
 
         if (RayCastDuckDetection())
+        {
             UI.GetComponent<UIController>().duckDetected = true;
+            if (rodRotationSpeed < 1)
+                rodRotationSpeed += rodRotationIncrease;
+        }
+            
         else
+        {
             UI.GetComponent<UIController>().duckDetected = false;
+            if (rodRotationSpeed > 0)
+                rodRotationSpeed -= rodRotationDecrease;
+        }
+        
 
+        //Debug.Log("Current speed: "+rodRotationSpeed);
     }
 
     void  GetInputLookRotation()
@@ -57,5 +82,10 @@ public class CameraController : MonoBehaviour
             
         }
         return ret;
+    }
+
+    void RotateHandle()
+    {
+        rodHandle.transform.Rotate(0, 0, -rodRotationSpeed);
     }
 }
